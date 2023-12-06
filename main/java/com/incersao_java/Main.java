8fba24d35fe26ebeda084f01c76e91c905d76171
Pedro Lucas Ponciano Silva <138740833+PedroLPS202@users.noreply.github.com>
2023-12-06 17:03:04 -0300

    Update Main.java

--- main/java/com/incersao_java/Main.java
+++ main/java/com/incersao_java/Main.java
@@ -1,4 +1,4 @@
-package com.incersao_java;
+/*package com.incersao_java;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,8 +13,18 @@ public class Main {
         String usuario = "root";
         String senha = "";
 
-        // Estabelece a conexão com o banco de dados MySQL
         try (Connection conexao = DriverManager.getConnection(url, usuario, senha)) {
+            try {
+                // Exclusão do aluno com todas as associações
+                excluirAlunoCursos(conexao, 1); // Substitua 1 pelo ID do aluno que deseja excluir
+
+                // Se não houve exceções, commita a transação
+                conexao.commit();
+            } catch (SQLException e) {
+                // Em caso de exceção, faz rollback para desfazer alterações
+                conexao.rollback();
+                e.printStackTrace();
+            }
 
             // Criação da tabela alunos
             try (Statement statement = conexao.createStatement()) {
@@ -40,48 +50,170 @@ public class Main {
                 System.out.println("Tabela cursos criada com sucesso.");
             }
 
-            String sqlAluno = "INSERT INTO alunos (id_aluno, nome, turma) VALUES (?, ?, ?)";
-            String sqlCurso = "INSERT INTO cursos (id_curso, nome_curso, instrutor) VALUES (?, ?, ?)";
+            String sqlAluno = "INSERT INTO alunos (nome, turma) VALUES (?, ?)";
+            String sqlCurso = "INSERT INTO cursos (nome_curso, instrutor) VALUES (?, ?)";
 
-            try (PreparedStatement preparedStatementAluno = conexao.prepareStatement(sqlAluno);
-                 PreparedStatement preparedStatementCurso = conexao.prepareStatement(sqlCurso)) {
+            try (PreparedStatement preparedStatementAluno = conexao.prepareStatement(sqlAluno, Statement.RETURN_GENERATED_KEYS);
+                 PreparedStatement preparedStatementCurso = conexao.prepareStatement(sqlCurso, Statement.RETURN_GENERATED_KEYS)) {
 
                 // Inserção de dados na tabela alunos
-                preparedStatementAluno.setInt(1, 1);
-                preparedStatementAluno.setString(2, "Lucas");
-                preparedStatementAluno.setString(3, "Turma A");
+                preparedStatementAluno.setString(1, "Lucas");
+                preparedStatementAluno.setString(2, "Turma A");
                 preparedStatementAluno.executeUpdate();
 
-                preparedStatementAluno.setInt(1, 2); // Usando ID diferente para o segundo aluno
-                preparedStatementAluno.setString(2, "Julia");
-                preparedStatementAluno.setString(3, "Turma B");
+                preparedStatementAluno.setString(1, "Julia");
+                preparedStatementAluno.setString(2, "Turma B");
                 preparedStatementAluno.executeUpdate();
 
                 // Inserção de dados na tabela cursos
-                preparedStatementCurso.setInt(1, 1);
-                preparedStatementCurso.setString(2, "Matematica");
-                preparedStatementCurso.setString(3, "Carlos");
+                preparedStatementCurso.setString(1, "Matematica");
+                preparedStatementCurso.setString(2, "Carlos");
                 preparedStatementCurso.executeUpdate();
 
-                preparedStatementCurso.setInt(1, 2); // Usando ID diferente para o segundo curso
-                preparedStatementCurso.setString(2, "Ciencias");
-                preparedStatementCurso.setString(3, "Ana");
+                preparedStatementCurso.setString(1, "Ciencias");
+                preparedStatementCurso.setString(2, "Ana");
                 preparedStatementCurso.executeUpdate();
 
             } catch (SQLException e) {
                 // Exibe informações detalhadas sobre o erro SQL
                 e.printStackTrace();
-            } catch (Exception e) {
-                // Exibe outras exceções
+            }
+        } catch (SQLException e) {
+            // Exibe informações detalhadas sobre o erro SQL
+            e.printStackTrace();
+        }
+
+        private static void excluirAlunoCursos(Connection conexao, int idAluno) {
+            try {
+                // Exclua o aluno
+                String sqlExcluirAluno = "DELETE FROM alunos WHERE id_aluno = ?";
+                try (PreparedStatement preparedStatementExcluirAluno = conexao.prepareStatement(sqlExcluirAluno)) {
+                    preparedStatementExcluirAluno.setInt(1, idAluno);
+                    preparedStatementExcluirAluno.executeUpdate();
+                }
+    
+                // Exclua os cursos associados ao aluno
+                String sqlExcluirCursosDoAluno = "DELETE FROM cursos WHERE id_aluno = ?";
+                try (PreparedStatement preparedStatementExcluirCursos = conexao.prepareStatement(sqlExcluirCursosDoAluno)) {
+                    preparedStatementExcluirCursos.setInt(1, idAluno);
+                    preparedStatementExcluirCursos.executeUpdate();
+                }
+    
+            } catch (SQLException e) {
+                // Exibe informações detalhadas sobre o erro SQL
+                e.printStackTrace();
+            }
+        }
+    }
+}*/
+
+package com.incersao_java;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/incersao_dados";
+        String usuario = "root";
+        String senha = "";
+
+        try (Connection conexao = DriverManager.getConnection(url, usuario, senha)) {
+            try {
+                // Exclusão do aluno com todas as associações
+                excluirAlunoCursos(conexao, 1); // Substitua 1 pelo ID do aluno que deseja excluir
+
+                // Se não houve exceções, commita a transação
+                conexao.commit();
+            } catch (SQLException e) {
+                // Em caso de exceção, faz rollback para desfazer alterações
+                conexao.rollback();
+                e.printStackTrace();
+            }
+
+            // Criação da tabela alunos
+            try (Statement statement = conexao.createStatement()) {
+                String sql = "CREATE TABLE IF NOT EXISTS alunos ("
+                        + "id_aluno INT AUTO_INCREMENT PRIMARY KEY,"
+                        + "nome VARCHAR(255),"
+                        + "turma VARCHAR(255)"
+                        + ")";
+                // Executa a instrução SQL para criar a tabela alunos
+                statement.executeUpdate(sql);
+                System.out.println("Tabela alunos criada com sucesso.");
+            }
+
+            // Criação da tabela cursos
+            try (Statement statement = conexao.createStatement()) {
+                String sql = "CREATE TABLE IF NOT EXISTS cursos ("
+                        + "id_curso INT AUTO_INCREMENT PRIMARY KEY,"
+                        + "nome_curso VARCHAR(255),"
+                        + "instrutor VARCHAR(255)"
+                        + ")";
+                // Executa a instrução SQL para criar a tabela cursos
+                statement.executeUpdate(sql);
+                System.out.println("Tabela cursos criada com sucesso.");
+            }
+
+            String sqlAluno = "INSERT INTO alunos (nome, turma) VALUES (?, ?)";
+            String sqlCurso = "INSERT INTO cursos (nome_curso, instrutor) VALUES (?, ?)";
+
+            try (PreparedStatement preparedStatementAluno = conexao.prepareStatement(sqlAluno, Statement.RETURN_GENERATED_KEYS);
+                 PreparedStatement preparedStatementCurso = conexao.prepareStatement(sqlCurso, Statement.RETURN_GENERATED_KEYS)) {
+
+                // Inserção de dados na tabela alunos
+                preparedStatementAluno.setString(1, "Lucas");
+                preparedStatementAluno.setString(2, "Turma A");
+                preparedStatementAluno.executeUpdate();
+
+                preparedStatementAluno.setString(1, "Julia");
+                preparedStatementAluno.setString(2, "Turma B");
+                preparedStatementAluno.executeUpdate();
+
+                // Inserção de dados na tabela cursos
+                preparedStatementCurso.setString(1, "Matematica");
+                preparedStatementCurso.setString(2, "Carlos");
+                preparedStatementCurso.executeUpdate();
+
+                preparedStatementCurso.setString(1, "Ciencias");
+                preparedStatementCurso.setString(2, "Ana");
+                preparedStatementCurso.executeUpdate();
+
+            } catch (SQLException e) {
+                // Exibe informações detalhadas sobre o erro SQL
                 e.printStackTrace();
             }
         } catch (SQLException e) {
             // Exibe informações detalhadas sobre o erro SQL
             e.printStackTrace();
-        } catch (Exception e) {
-            // Exibe outras exceções
+        }
+    }
+
+    private static void excluirAlunoCursos(Connection conexao, int idAluno) {
+        try {
+            // Exclua o aluno
+            String sqlExcluirAluno = "DELETE FROM alunos WHERE id_aluno = ?";
+            try (PreparedStatement preparedStatementExcluirAluno = conexao.prepareStatement(sqlExcluirAluno)) {
+                preparedStatementExcluirAluno.setInt(1, idAluno);
+                preparedStatementExcluirAluno.executeUpdate();
+            }
+
+            // Exclua os cursos associados ao aluno
+            String sqlExcluirCursosDoAluno = "DELETE FROM cursos WHERE id_aluno = ?";
+            try (PreparedStatement preparedStatementExcluirCursos = conexao.prepareStatement(sqlExcluirCursosDoAluno)) {
+                preparedStatementExcluirCursos.setInt(1, idAluno);
+                preparedStatementExcluirCursos.executeUpdate();
+            }
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 }
+
+
     
